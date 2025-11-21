e33c06d73db1bb79037a469b124b3902b16bb0db
NizoDotNet <mmmdovnizami.official@gmail.com>
2025-11-21 21:57:29 +0400

    need write simple service for incapsulation of prediction by img

--- server/main.py
+++ server/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi import File, UploadFile
 import tensorflow as tf
 from keras.models import load_model
 
@@ -10,3 +11,8 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+@app.post('/skin-can/detector')
+def skin_can_detect(img: UploadFile):
+    img = img.file.read()
+    
+    return { 'result': '' }
